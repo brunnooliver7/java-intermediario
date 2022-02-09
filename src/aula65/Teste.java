001395e6be30d928d13b4beb4ac229bee8642667
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-02-08 21:35:58 -0300

    aula65

---
+++ src/aula65/Teste.java
@@ -0,0 +1,22 @@
+package aula65;
+
+import aula65.InformacaoAula;
+
+@InformacaoAula(
+	autor = "Loiane",
+	aulaNumero = 65,
+	blog = "loiane.com"
+)
+public class Teste {
+
+	@InformacaoAula(
+			autor = "Loiane",
+			aulaNumero = 65,
+			blog = "loiane.com"
+		)
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
